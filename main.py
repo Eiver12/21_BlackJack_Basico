1ca4a02c81265f68c4acde3f39b3c575978fa905
Eiver12 <eivercarvajal12@gmail.com>
2026-01-29 10:28:26 -0500

    ultima modificacion de impresion en la funcion main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     print("aqui va la logica")
     print("Aqui puede ir el control")
+    print("pueden ir las variables")
+    print("anexando un comentario")
 
 if __name__ == "__main__":
     main()
